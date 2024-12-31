fc15e95001df584a24f7c2b9511937278d097b1f
kimjisoo <sallysooo@naver.com>
2025-01-01 01:06:24 +0900

    access setting

--- upload.php
+++ upload.php
@@ -1,7 +1,17 @@
 <?php
+    // ID용 세션 시작
+    session_start();
+
+    // 참가자별 고유 업로드 디렉토리
+    $participantDir = __DIR__ . '/uploads/' . session_id();
+    if(!is_dir($participantDir)){
+        mkdir($participantDir, 0777, true);
+    }
+
+
     // 파일 업로드 처리
     if ($_SERVER['REQUEST_METHOD']==='POST'){
-        $uploadDir = __DIR__ . '/uploads/';
+        $uploadDir = $participantDir . '/';
         $uploadFile = $uploadDir . basename($_FILES['image']['name']);
         
         // 허용된 확장자 목록
@@ -24,7 +34,7 @@
 
         // 파일 업로드 처리
         if (move_uploaded_file($_FILES['image']['tmp_name'], $uploadFile)) {
-            echo "파일이 업로드되었습니다: <a href='uploads/" . htmlspecialchars($_FILES['image']['name']) . "'>" . htmlspecialchars($_FILES['image']['name']) . "</a>";
+            echo "파일이 업로드되었습니다: <a href='uploads/" . session_id() . '/' . htmlspecialchars($_FILES['image']['name']) . "'>" . htmlspecialchars($fileName) . "</a>";
         } else {
             echo "파일 업로드에 실패했습니다.";
         }
